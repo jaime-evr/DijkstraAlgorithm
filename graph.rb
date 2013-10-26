7f5fc365009ce02cf3915e331437ca745695bf83
jaime <jaime.victoria.90@gmail.com>
2013-10-25 23:20:10 -0500

    added tests and main files

--- graph.rb
+++ graph.rb
@@ -29,28 +29,3 @@ class Graph
     @nodes[index].neighbors = neighbors
   end
 end
-
-g = Graph.new 6, 0, 5
-
-neighborhood = [
-  [{node: 2, val: 7}, {node: 3, val: 9}, {node: 6, val: 14}],
-  [{node: 1, val: 7}, {node: 3, val: 10}, {node: 4, val: 15}],
-  [{node: 1, val: 9}, {node: 2, val: 10}, {node: 4, val: 11}, {node: 6, val: 2}],
-  [{node: 2, val: 15}, {node: 3, val: 11},{node: 5, val: 6}],
-  [{node: 4, val: 6}, {node: 6, val: 9}],
-  [{node: 5, val: 9}, {node: 3, val: 2},{node: 1, val: 14}]
-]
-
-g.set_nodes
-g.set_initial
-
-g.total.times do |index|
-  g.set_node_neighbors(index, neighborhood)
-end
-
-puts g.nodes
-
-d = Dijkstra.new(g)
-unvisited = d.get_unvisited_set
-
-puts g
---
+++ tests.rb
@@ -0,0 +1,41 @@
+require './graph'
+require './node'
+require './dijkstra'
+
+describe Dijkstra do
+  before do
+    @g = Graph.new 6, 0, 5
+    @d = Dijkstra.new @g
+  end
+
+  it 'should have a graph' do  
+    @d.graph.should_not be nil
+  end
+
+  it 'should return unvisited set' do
+    @d.get_unvisited_set.should_not be nil
+  end
+end
+
+describe Graph do
+  before do
+    @g = Graph.new 6, 0, 5
+  end
+
+  it 'should set nodes' do
+    @g.set_nodes
+    @g.nodes.length.should eq 6
+  end
+
+  it 'should set the initial node' do
+    @g.set_nodes
+    @g.set_initial
+    @g.nodes[0].initial.should be true
+    @g.nodes[0].value.should eq 0
+    @g.nodes[0].current.should be true
+    @g.nodes[0].visited.should be true
+  end
+end
+
+describe Node do
+end
