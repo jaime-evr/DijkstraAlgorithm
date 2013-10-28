11fb62ba89858a86f8a0d56ed81dab44e1b17963
jaime <jaime.victoria.90@gmail.com>
2013-10-27 19:43:39 -0600

    added new method to evaluate node neighbors

--- dijkstra.rb
+++ dijkstra.rb
@@ -1,25 +1,35 @@
-require 'pry'
 class Dijkstra
-  attr_accessor :graph, :unvisited_set
+  attr_accessor :graph, :unvisited_set, :total_value
 
   def initialize(graph)
+    @total_value = 0
     @graph = graph
     @unvisited_set = {}
   end
 
-  def get_unvisited_set
+  def unvisited_set
     @graph.nodes.each do |index, node|
       @unvisited_set[index] = node unless node.visited == true
     end
     @unvisited_set
   end
 
-  def get_current_node
+  def current_node
     @graph.nodes.each { |index, n| return n if n.current == true }
   end
 
-  def get_shortest_path
-    current = get_current_node
-    puts current.neighbors
+  def shortest_path
+    evaluate_node(current_node) 
+    binding.pry
   end
+
+  def evaluate_node(node)
+    node.neighbors.each do |neighbor|
+      value = neighbor[:cost] + @total_value
+      if value < @graph.nodes[neighbor[:node]].value do
+        @graph.nodes[neighbor[:node]].value = value
+      end
+    end
+  end
+  
 end
